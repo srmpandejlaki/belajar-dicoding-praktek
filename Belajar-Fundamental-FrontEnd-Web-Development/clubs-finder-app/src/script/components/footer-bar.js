30530c62350807296e8283101b6508e5ad263633
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-18 10:07:40 +0800

    tmbh footer-bar.js

---
+++ Belajar-Fundamental-FrontEnd-Web-Development/clubs-finder-app/src/script/components/footer-bar.js
@@ -0,0 +1,45 @@
+class FooterBar extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._style = document.createElement("style");
+  }
+
+  updateStyle() {
+    this._style.textContent = `
+        :host {
+        display: block;
+        }
+        
+        div {
+        padding: 24px 20px;
+        
+        text-align: center;
+        }
+        `;
+  }
+
+  _emptyContent() {
+    this._shadowRoot.innerHTML = "";
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this._emptyContent();
+    this._updateStyles();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+        <div>
+            Club Finder &copy; 2024
+        </div>
+        `;
+  }
+}
